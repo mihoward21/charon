22106282d45d0d11cc2d539f47e0f60bc67d7c18
Mike <mikeh09@gmail.com>
2020-10-04 19:50:02 -0700

    button component

---
+++ my-app/src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button onClick={this.props.onClick}>
+                {this.props.buttonText}
+            </button>
+        );
+    }
+}
+
+export default Button;
